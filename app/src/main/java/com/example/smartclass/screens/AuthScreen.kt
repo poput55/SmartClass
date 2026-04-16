541e0b8d8eaadf5e4b8b043853fd235b0417dde2
poput <159613160+poput55@users.noreply.github.com>
2026-04-16 14:44:17 +0300

    fix

--- app/src/main/java/com/example/smartclass/screens/AuthScreen.kt
+++ app/src/main/java/com/example/smartclass/screens/AuthScreen.kt
@@ -13,13 +13,10 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
